de915ccfd0e4251e63a2e96dcad55634cfdddc5c
hungchun0201 <penguin7296@gmail.com>
2019-12-17 20:16:42 +0800

    add search opt

--- crawl.py
+++ crawl.py
@@ -4,7 +4,7 @@ import re
 import pandas as pd
 import time
 import argparse
-from collections import OrderedDict
+# from collections import OrderedDict
 
 
 class Crawler():
@@ -27,10 +27,25 @@ class Crawler():
             "20:15": "C",
             "21:10": "D",
         }
-        OrderedDict(sorted(self.periodDict.items(), key=lambda t: t[0]))
+        self.periodKey = list(self.periodDict.keys())
+        # OrderedDict(sorted(self.periodDict.items(), key=lambda t: t[0]))
         self.parser()
 
     def parser(self):
+
+        def parseOptArgs(str):
+            if str == None:
+                return {}
+            pieces = str.split(',')
+            opts = {}
+            for p in pieces:
+                if '=' in p:
+                    key, val = p.split('=')
+                else:
+                    key, val = p, 1
+                opts[key] = val
+            return opts
+
         parser = argparse.ArgumentParser(prog='python3 crawl.py', description='''
 
         This is a program that can crawl data of classes in next semester from
@@ -64,10 +79,28 @@ class Crawler():
             choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, "A",
                      "B", "共同", "普通", "新生", "綜合", "博雅", "%"]
         )
+        parser.add_argument(
+            "--search-opt", dest="searchOpt", help='''Comma separated values to specifiy search options e.g. "Title=積體電路,Classroom=電二"
+            The avaliable args include:
+                "Id": Curriculum Identity Number
+                "Class": The class number. If the course is teached by only one teacher, it is set to 00
+                "Title": Course title
+                "Instructor": Teacher name
+                "Classroom": Schedule Classroom
+                "Time": The time of course
+            
+            For example, if you type "--search-opt Title=積體電路,Classroom=電二", you may get the following result:
+                     Id Class        Title Instructor Classroom    Time
+            0  943U0010    00       積體電路測試        李建模     電二146  二2,3,4
+            1  921U9590    00  電力電子與積體電路控制        陳景然     電二225  二7,8,9
+            2  943U0120    00     射頻積體電路設計        陳怡然     電二104  三2,3,4
+            3  90140500    00       積體電路設計        盧奕璋     電二229  三7,8,9
+            4  942U0120    00     微波積體電路專題        林坤佑     電二101  四7,8,9
+            '''
+        )
 
         args = parser.parse_args()
-        if(args.tor):
-            pass
+        args.searchOpt = parseOptArgs(args.searchOpt)
         self.args = args
 
     def transformTime(self, time):
@@ -79,16 +112,10 @@ class Crawler():
             return day+self.periodDict[periods[0]]
         else:  # more than one session
             return_str = ""
-            addToStr = False
-            for session in self.periodDict:
-                if(session == periods[0]):
-                    addToStr = True
-                    return_str += self.periodDict[session]
-                elif(session == periods[1]):
-                    return_str += (","+self.periodDict[session])
-                    break
-                elif(addToStr == True):
-                    return_str += (","+self.periodDict[session])
+            start = self.periodKey.index(periods[0])
+            end = self.periodKey.index(periods[1])+1
+            return_str = ','.join(self.periodDict[k]
+                                  for k in self.periodKey[start:end])
 
             return day+return_str
 
@@ -104,6 +131,18 @@ class Crawler():
         # print(last_row.findChildren())
         self.args.page = len(last_row.findChildren())
 
+    def checkSearchOpts(self, dict):
+        if(not self.args.searchOpt):
+            return True
+        else:
+            for key in self.args.searchOpt:
+                if(self.args.searchOpt[key] not in dict[key]):
+                    return False
+            else:
+                print(dict)
+                return True
+
+
     def crawl(self):
 
         class_info = []
@@ -113,8 +152,16 @@ class Crawler():
         for week in range(1, 7):
             for page in range(1, self.args.page+1):
                 doc = requests.get(
-                    'http://gra206.aca.ntu.edu.tw/classrm/index.php/acarm/webcr-use1-new?Type=1&page={}&SYearDDL={}&BuildingDDL={}&Week={}&Capacity=1&SelectButton=%E6%9F%A5%E8%A9%A2'
-                    .format(page, self.args.semester, self.args.building, week))
+                    'http://gra206.aca.ntu.edu.tw/classrm/index.php/acarm/webcr-use1-new',
+                    params={
+                        'Type': '1',
+                        'page': str(page),
+                        'SYearDDL': self.args.semester,
+                        'BuildingDDL': self.args.building,
+                        'Week': str(week),
+                        'Capacity': '1',
+                        'SelectButton': '%E6%9F%A5%E8%A9%A2'
+                    })
                 doc.encoding = 'UTF-8'
                 doc = doc.text
                 soup = BeautifulSoup(doc, 'html.parser')
@@ -123,10 +170,10 @@ class Crawler():
                         sub_table = sub_table[0].text
                         sub_table = re.sub(r"[ \t\r\f\v]+", "", sub_table)
                         newCourse = sub_table.split(
-                            '\n')     # split string with \n       
-                        newCourse = [x for x in newCourse if x]# remove '' element in list
+                            '\n')     # split string with \n
+                        # remove '' element in list
+                        newCourse = [x for x in newCourse if x]
                         first_str = newCourse[0].split("：")[0]
-
                         if(first_str != '課程識別碼'):
                             continue
 
@@ -137,36 +184,40 @@ class Crawler():
                         elif(len(newCourse) < 11):
                             # bad course
                             continue
+                        # print("oo")
+                        dict = {
+                            "Id": newCourse[1],#Curriculum Identity Number
+                            "Class": newCourse[3],
+                            "Title": newCourse[5],#Course title
+                            "Instructor": newCourse[7],
+                            "Classroom": newCourse[9],#Schedule Classroom
+                            "Time": newCourse[11],
+                        }
+                        dict["Time"] = self.transformTime(dict["Time"])
 
-                        newCourse[11] = self.transformTime(newCourse[11])
+                        if(not self.checkSearchOpts(dict)):
+                            continue
 
                         # check whether is the same class
                         endloop = False
-                        for dict in class_info:
-                            if(dict["Course title"] == newCourse[5] and dict["Instructor"] == newCourse[7]):
+
+                        for ori_dict in class_info:
+                            if(ori_dict["Title"] == newCourse[5] and ori_dict["Instructor"] == newCourse[7]):
                                 endloop = True
-                                if(newCourse[11] in dict["time"]):
+                                if(dict["Time"] in ori_dict["Time"]):
                                     break
                                 else:
-                                    dict["time"] = dict["time"] + \
-                                        ' '+newCourse[11]
+                                    ori_dict["Time"] = ori_dict["Time"] + \
+                                        ' '+dict["Time"]
                                     break
                         if(endloop):
                             continue
 
-                        dict = {
-                            "Curriculum Identity Number": newCourse[1],
-                            "Class": newCourse[3],
-                            "Course title": newCourse[5],
-                            "Instructor": newCourse[7],
-                            "Schedule Classroom": newCourse[9],
-                            "time": newCourse[11],
-                        }
+                        
                         class_info.append(dict)
                     except:
                         continue
-                print("========== Got {} courses information, until Page {} and Day {} ==========".format(
-                    len(class_info), page, week))
+                print("========== Got", len(class_info), "courses information, until Page", page, "and Day", week ,"==========")
                 time.sleep(self.args.delay)
 
         select_df = pd.DataFrame(class_info)
