97a40399913d4e8b98f79d719564d83ff891863e
hungchun0201 <penguin7296@gmail.com>
2019-12-19 01:02:29 +0800

    complete help message

--- crawl.py
+++ crawl.py
@@ -4,7 +4,6 @@ import re
 import pandas as pd
 import time
 import argparse
-# from collections import OrderedDict
 
 
 class Crawler():
@@ -54,13 +53,13 @@ class Crawler():
         organize the curriculum for next semester in advance. The reference link is
         http://gra206.aca.ntu.edu.tw/classrm/index.php/acarm/webcr-use1-new.
 
-        ''', epilog="Designed by Hung-Chun,Lin.")
+        ''', epilog="Designed by Hung-Chun,Lin.", formatter_class=argparse.RawTextHelpFormatter)
 
         parser.add_argument(
             "--semester", help="Select the semester you want to query", default="1082")
 
         parser.add_argument(
-            "--delay-time", help="Set the delay time between each request", type=float, default=0.5, dest="delay")
+            "--delay-time", help="Set the delay time between each request", type=float, default=0, dest="delay")
 
         parser.add_argument(
             "--tor", help="(NOT IMPLETEMENTED)Try to use tor proxy to prevent from blocking IP by the website", action="store_true")
@@ -69,34 +68,36 @@ class Crawler():
             "-p", "--page", help="Assign the maximum page", type=int)
 
         parser.add_argument(
-            "-s", "--save", help="Store the result. You can specify your filename.\
-            Please use .xls or .xlsx as filename extension.", const="result.xls", action="store", nargs="?")
+            "-s", "--save", help='''Store the result. You can specify your filename.
+Please use .xls or .xlsx as filename extension.''', const="result.xls", action="store", nargs="?")
 
         parser.add_argument(
-            "--building", help="Specify the building you want to query. \
-            If the building belongs to a college(ex:College of Electrical Engineering and Computer Science), \
-            use the code of that college(ex:9),or simply type the name of the building(ex:博雅).", default=9,
+            "-b", help='''Specify the building you want to query.
+If the building belongs to a college(ex:College of Electrical Engineering and Computer Science),
+use the code of that college(ex:9),or simply type the name of the building(ex:博雅).
+The percentage sign means search for all buildings.''', default=9,
             choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, "A",
                      "B", "共同", "普通", "新生", "綜合", "博雅", "%"]
         )
         parser.add_argument(
-            "--search-opt", dest="searchOpt", help='''Comma separated values to specifiy search options e.g. "Title=積體電路,Classroom=電二"
-            The avaliable args include:
-                "Id": Curriculum Identity Number
-                "Class": The class number. If the course is teached by only one teacher, it is set to 00
-                "Title": Course title
-                "Instructor": Teacher name
-                "Classroom": Schedule Classroom
-                "Time": The time of course
-            
-            For example, if you type "--search-opt Title=積體電路,Classroom=電二", you may get the following result:
-                     Id Class        Title Instructor Classroom    Time
-            0  943U0010    00       積體電路測試        李建模     電二146  二2,3,4
-            1  921U9590    00  電力電子與積體電路控制        陳景然     電二225  二7,8,9
-            2  943U0120    00     射頻積體電路設計        陳怡然     電二104  三2,3,4
-            3  90140500    00       積體電路設計        盧奕璋     電二229  三7,8,9
-            4  942U0120    00     微波積體電路專題        林坤佑     電二101  四7,8,9
-            '''
+            "--search-opt", dest="searchOpt", help='''
+Comma separated values to specifiy search options e.g. "Title=積體電路,Classroom=電二"
+The avaliable args include:
+    "Id": Curriculum Identity Number
+    "Class": The class number. If the course is teached by only one teacher, it is set to 00
+    "Title": Course title
+    "Instructor": Teacher name
+    "Classroom": Schedule Classroom
+    "Time": The time of course
+
+For example, if you type "--search-opt Title=積體電路,Classroom=電二", you may get the following result:
+            Id Class        Title Instructor Classroom    Time
+0  943U0010    00       積體電路測試        李建模     電二146  二2,3,4
+1  921U9590    00  電力電子與積體電路控制        陳景然     電二225  二7,8,9
+2  943U0120    00     射頻積體電路設計        陳怡然     電二104  三2,3,4
+3  90140500    00       積體電路設計        盧奕璋     電二229  三7,8,9
+4  942U0120    00     微波積體電路專題        林坤佑     電二101  四7,8,9
+        '''
         )
 
         args = parser.parse_args()
@@ -142,14 +143,13 @@ class Crawler():
                 print(dict)
                 return True
 
-
     def crawl(self):
 
         class_info = []
         if(not self.args.page):
             self.getMaximumPage()
 
-        for week in range(1, 7):
+        for week in range(1, 6):
             for page in range(1, self.args.page+1):
                 doc = requests.get(
                     'http://gra206.aca.ntu.edu.tw/classrm/index.php/acarm/webcr-use1-new',
@@ -186,11 +186,11 @@ class Crawler():
                             continue
                         # print("oo")
                         dict = {
-                            "Id": newCourse[1],#Curriculum Identity Number
+                            "Id": newCourse[1],  # Curriculum Identity Number
                             "Class": newCourse[3],
-                            "Title": newCourse[5],#Course title
+                            "Title": newCourse[5],  # Course title
                             "Instructor": newCourse[7],
-                            "Classroom": newCourse[9],#Schedule Classroom
+                            "Classroom": newCourse[9],  # Schedule Classroom
                             "Time": newCourse[11],
                         }
                         dict["Time"] = self.transformTime(dict["Time"])
@@ -201,23 +201,23 @@ class Crawler():
                         # check whether is the same class
                         endloop = False
 
-                        for ori_dict in class_info:
-                            if(ori_dict["Title"] == newCourse[5] and ori_dict["Instructor"] == newCourse[7]):
+                        for old_dict in class_info:
+                            if(old_dict["Id"] == dict["Id"] and old_dict["Class"] == dict["Class"]):
                                 endloop = True
-                                if(dict["Time"] in ori_dict["Time"]):
+                                if(dict["Time"] in old_dict["Time"]):
                                     break
                                 else:
-                                    ori_dict["Time"] = ori_dict["Time"] + \
+                                    old_dict["Time"] = old_dict["Time"] + \
                                         ' '+dict["Time"]
                                     break
                         if(endloop):
                             continue
 
-                        
                         class_info.append(dict)
                     except:
                         continue
-                print("========== Got", len(class_info), "courses information, until Page", page, "and Day", week ,"==========")
+                print("========== Got", len(
+                    class_info), "courses information, until Page", page, "and Day", week, "==========")
                 time.sleep(self.args.delay)
 
         select_df = pd.DataFrame(class_info)
--- readme.md
+++ readme.md
@@ -12,7 +12,7 @@ http://gra206.aca.ntu.edu.tw/classrm/index.php/acarm/webcr-use1-new.
 <a target="_blank" href="https://www.python.org/downloads/" title="Python version"><img src="https://img.shields.io/badge/python-%3E=_3.7-green.svg"></a> and the following libraries are required.
 
 
-> <img src="https://img.shields.io/badge/python-requests %7C beautifulSoup4 %7C pandas %7C collections %7C argparse-blue">
+> <img src="https://img.shields.io/badge/python-requests %7C beautifulSoup4 %7C pandas %7C argparse-blue">
 
 
 
@@ -27,20 +27,49 @@ optional arguments:
   -h, --help            show this help message and exit
   --semester SEMESTER   Select the semester you want to query
   --delay-time DELAY    Set the delay time between each request
-  --tor                 (NOT IMPLEMENTED)Try to use tor proxy to prevent
-                        from blocking IP by the website
+  --tor                 (NOT IMPLETEMENTED)Try to use tor proxy to prevent from blocking IP by the website
   -p PAGE, --page PAGE  Assign the maximum page
   -s [SAVE], --save [SAVE]
                         Store the result. You can specify your filename.
                         Please use .xls or .xlsx as filename extension.
-  --building {1,2,3,4,5,6,7,8,9,A,B,共同,普通,新生,綜合,博雅,%}
-                        Specify the building you want to query. If the
-                        building belongs to a college(ex:College of Electrical
-                        Engineering and Computer Science), use the code of
-                        that college(ex:9),or simply type the name of the
-                        building(ex:博雅).
+  -b {1,2,3,4,5,6,7,8,9,A,B,共同,普通,新生,綜合,博雅,%}
+                        Specify the building you want to query.
+                        If the building belongs to a college(ex:College of Electrical Engineering and Computer Science),
+                        use the code of that college(ex:9),or simply type the name of the building(ex:博雅).
+                        The percentage sign means search for all buildings.
+  --search-opt SEARCHOPT
+
+                        Comma separated values to specifiy search options e.g. "Title=積體電路,Classroom=電二"
+                        The avaliable args include:
+                            "Id": Curriculum Identity Number
+                            "Class": The class number. If the course is teached by only one teacher, it is set to 00
+                            "Title": Course title
+                            "Instructor": Teacher name
+                            "Classroom": Schedule Classroom
+                            "Time": The time of course
+
+                        For example, if you type "--search-opt Title=積體電路,Classroom=電二", you may get the following result:
+                                    Id Class        Title Instructor Classroom    Time
+                        0  943U0010    00       積體電路測試        李建模     電二146  二2,3,4
+                        1  921U9590    00  電力電子與積體電路控制        陳景然     電二225  二7,8,9
+                        2  943U0120    00     射頻積體電路設計        陳怡然     電二104  三2,3,4
+                        3  90140500    00       積體電路設計        盧奕璋     電二229  三7,8,9
+                        4  942U0120    00     微波積體電路專題        林坤佑     電二101  四7,8,9
+
+
 ```
 You can view all help message by typing
 ```
 python3 crawl.py -h
 ```
+## Examples
+
+If you want to find the class whose name is 機器學習 in EECS and store the result to excel with filename <code>ML.xls</code>, you can use
+```
+python3 crawl.py --search-opt Title=機器學習 --save ML.xls -b 9
+```
+or maybe you just want to find the classes teached in Bioresources and Agriculture college,you can just type
+```
+python3 crawl.py -b 6
+```
+
